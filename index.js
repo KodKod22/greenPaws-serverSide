a2c617fab13c98b8613dbe2749e674e803a56e1b
Kody agin peters <131360013+KodKod22@users.noreply.github.com>
2025-06-24 19:51:07 +0300

    creating the starting files

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+        'Content-Type': 'application/json'
+    });
+    next();
+});
+
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+})
