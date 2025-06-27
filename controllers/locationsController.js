6f239db599153fd60b9265fa9449f5e8deb93e14
Kody agin peters <131360013+KodKod22@users.noreply.github.com>
2025-06-27 13:08:44 +0300

    creating the add Bottlesl and a middwer function the check the req if is valid req

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -5,9 +5,22 @@ exports.locationsController = {
             const result = await dbConnection.query('SELECT * FROM Locations')
             res.status(200).json(result.rows);
         }catch(err){
-            console.error("error fetching locations:",err);
+            
             res.status(500).json({error:'Database error'})
         }
 
+    },
+    async addBottles(req,res) {
+        try{
+            const { userID, locationId, bottleCount } = req.body;   
+            const result = await dbConnection.query('INSERT INTO recycleactivity (userid, locationid, bottlecount,logdate) VALUES ($1,$2,$3,NOW())',[userID, locationId, bottleCount]);
+            res.status(200).json({ message: "Bottles added", entry: result.rows[0] });
+            const REDUCTION_PER_BOTTLE = 2; 
+            const reduction = bottleCount * REDUCTION_PER_BOTTLE;
+
+            await dbConnection.query(`UPDATE Locations SET foodcapacity = GREATEST(foodcapacity - $1, 0) WHERE locationsId = $2`, [reduction, locationId]);
+        }catch(err){
+            res.status(500).json({ error: 'Server error' });
+        }
     }
 }
--- routers/locationsRouter.js
+++ routers/locationsRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const locationsRouter = new Router();
 const { locationsController } = require('../controllers/locationsController.js');
+const  middleware  = require('../middleware/checkAddbottels.js');
 
 locationsRouter.get('/Locations',locationsController.getLocations);
+locationsRouter.post('/addBottles',middleware,locationsController.addBottles)
 
 module.exports = { locationsRouter }
